46d304ee073ff1bf257b1de82b2a155d40f9b270
G3bE <32980656+G3bE@users.noreply.github.com>
2018-10-04 22:55:09 +0200

    Deleted time out of help
    Time isn't working? So I just deleted it...

--- commands/help.py
+++ commands/help.py
@@ -2,7 +2,7 @@ class command:
     """Still hardcoded too. I'm working on this."""
 
     def exec(self):
-        command_list = [["exit", "exits the console"], ["time", "gives the current time"], ["ls", "lists all files in the current directory"], ["cd", "changes the current directory"], ["about", "shows some info about bash.py"], ["help", "shows this list"]]
+        command_list = [["exit", "exits the console"], ["ls", "lists all files in the current directory"], ["cd", "changes the current directory"], ["about", "shows some info about bash.py"], ["help", "shows this list"]]
 
         for elem in command_list:
             print(elem[0] + "\t" + elem[1])
