0029d9380a2081468a1adc915d2b99369da0c6d1
G3bE <32980656+G3bE@users.noreply.github.com>
2018-10-12 19:02:29 +0200

    Made everything a bit more logic + inbuild mode
    print_lines --> print_lines
                       |-> print_lines_delay
    
    init file for utility and commands
    
    init from main is now in the commands class
    
    new inbuild mode: passes everything to the system

--- main.py
+++ main.py
@@ -4,46 +4,38 @@ import shlex
 
 
 class Commands:
-    """Yes, this is hard-coded. I am still learning."""
-    from commands import rm
-    from commands import help
-    from commands import exit
-    from commands import cd
-    from commands import ls
-    from commands import about
+    import commands
 
+    os.environ['directory'] = "/"
 
-os.environ['directory'] = "/"
+    def __init__(self):
+        print("""
+      _               _                   
+     | |             | |                  
+     | |__   __ _ ___| |__    _ __  _   _ 
+     | '_ \ / _` / __| '_ \  | '_ \| | | |
+     | |_) | (_| \__ \ | | |_| |_) | |_| |
+     |_.__/ \__,_|___/_| |_(_) .__/ \__, |
+                             | |     __/ |
+     welcome to bash.py!     |_|    |___/ 
 
+     For a list of all commands, type 'help'
+    """)
+        userin = ""
+        while userin != "exit":
+            userin = input(platform.uname()[0] + "@" + platform.uname()[1] + os.environ['directory'] + "$ ")
 
-def __init__():
-    print("""
-  _               _                   
- | |             | |                  
- | |__   __ _ ___| |__    _ __  _   _ 
- | '_ \ / _` / __| '_ \  | '_ \| | | |
- | |_) | (_| \__ \ | | |_| |_) | |_| |
- |_.__/ \__,_|___/_| |_(_) .__/ \__, |
-                         | |     __/ |
- welcome to bash.py!     |_|    |___/ 
- 
- For a list of all commands, type 'help'
-""")
-    userin = ""
-    while userin != "exit":
-        userin = input(platform.uname()[0] + "@" + platform.uname()[1] + os.environ['directory'] + "$ ")
+            userin = shlex.split(userin)
+            if len(userin) > 0:
+                invoke, *args = userin
 
-        userin = shlex.split(userin)
-        if len(userin) > 0:
-            invoke, *args = userin
-
-            try:
-                command = getattr(Commands, invoke)
-            except AttributeError:
-                print(invoke + ": Command not found")
-                continue
-            command.Command.exec(args)
+                try:
+                    command = getattr(self.commands, invoke)
+                except AttributeError:
+                    print(invoke + ": Command not found")
+                    continue
+                command.Command.exec(args)
 
 
 if __name__ == '__main__':
-    __init__()
+    commandclass = Commands()
--- utility/cbc.py
+++ utility/cbc.py
@@ -2,10 +2,16 @@ import sys
 import time
 
 
-def print_lines(lines):
+def print_lines_delay(lines):
     for line in lines:
         for i in range(len(line) + 1):
             sys.stdout.write('\r' + line[:i])
             time.sleep(.05)
         time.sleep(.2)
         sys.stdout.write("\n")
+
+
+def print_lines(lines):
+    for line in lines:
+        for i in range(len(line) + 1):
+            sys.stdout.write('\r' + line[:i])
